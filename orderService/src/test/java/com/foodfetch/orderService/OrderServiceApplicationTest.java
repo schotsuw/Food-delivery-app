6e707cf2f6751acfd34b79c514bb940f992d31fc
masonlalonde17 <74019181+masonlalonde17@users.noreply.github.com>
2025-03-31 16:59:02 -0300

    context added

--- orderService/src/test/java/com/foodfetch/orderService/OrderServiceApplicationTest.java
+++ orderService/src/test/java/com/foodfetch/orderService/OrderServiceApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-    "spring.data.mongodb.uri=mongodb://localhost:27017/test"  // <-- Valid dummy URI
+    "spring.data.mongodb.uri=mongodb://localhost:27017/test"  //dummy mongo setup
 })
 public class OrderServiceApplicationTest {
-
+//tests that spring boot can launch
     @Test
     void contextLoads() {
     }
